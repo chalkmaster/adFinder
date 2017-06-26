1dc9b04e73679da408290912fc488789359a4b7a
Charles Fortes <chalkmaster@gmail.com>
2017-06-26 19:00:03 -0300

    Routers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ const port = 8081;
 
 app.use(express.static('public'));
 
+app.post('/ad/', (req, resp) => {
+
+});
+
+app.put('/ad/', (req, resp) => {
+
+});
+
+app.get('/ad/:id', (req, resp) => {
+
+});
+
+app.delete('/ad/:id', (req, resp) => {
+
+});
+
+app.get('/ad/search', (req, resp) => {
+
+});
+
 app.listen(port, () => {
     console.log(`listening at ${port}`);
 });
