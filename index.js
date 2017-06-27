1838bc21f975962d2e84549701bcdb90328645f3
Charles Fortes <chalkmaster@gmail.com>
2017-06-27 19:06:57 -0300

    Controllers Running

--- index.js
+++ index.js
@@ -1,77 +1,46 @@
-const ArRepository = require('./AdRepository').AdRepository;
+'use strict'
+const AdRepository = require('./AdRepository').AdRepository;
+const bodyParser = require("body-parser");
 const express = require('express');
 const app = express();
 const port = (process.env.PORT || 5000);
+const repository = new AdRepository();
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.post('/ad/', (req, resp) => {
-    let newAd = {
-        name: req.body.name,
-        id: 0,
-        description: req.body.description,
-        region: req.body.region,
-        category: req.body.category,
-        contacts: {
-            phone: req.body.phone,
-            email: req.body.email,
-            site: req.body.site,
-        },
-        medias: {
-            photos: [
-                'https://s-media-cache-ak0.pinimg.com/736x/e5/a0/69/e5a06942fa42823c88be5f3a834e063d--fantastic-art-bat-family.jpg',
-                'https://lc-www-live-s.legocdn.com/r/www/r/catalogs/-/media/catalogs/characters/dc/mugshots/mugshot%202016/76061_1to1_mf_batman_336.png?l.r2=-798905063',
-            ]
-        },
-    }
-
-    const repository = new AdRepository();
+    let newAd = req.body;
+    newAd.id = 0;
     repository.save(newAd);
     resp.end();
 });
 
 app.put('/ad/', (req, resp) => {
-    let adToUpdate = {
-        name: req.body.name,
-        id: req.body.id,
-        description: req.body.description,
-        region: req.body.region,
-        category: req.body.category,
-        contacts: {
-            phone: req.body.phone,
-            email: req.body.email,
-            site: req.body.site,
-        },
-        medias: {
-            photos: [
-                'https://s-media-cache-ak0.pinimg.com/736x/e5/a0/69/e5a06942fa42823c88be5f3a834e063d--fantastic-art-bat-family.jpg',
-                'https://lc-www-live-s.legocdn.com/r/www/r/catalogs/-/media/catalogs/characters/dc/mugshots/mugshot%202016/76061_1to1_mf_batman_336.png?l.r2=-798905063',
-            ]
-        },
-    }
-
-    const repository = new AdRepository();
+    let adToUpdate = req.body;
     repository.save(adToUpdate);
     resp.end();
 });
 
-app.get('/ad/:id', (req, resp) => {
-    const repository = new AdRepository();
-    const id = parseInt(req.param("id",0));
+app.get('/ad/:id', (req, resp) => {    
+    const id = parseInt(req.params.id);
     let found = repository.findById(id);
-    resp.write(found);
+    if(found)
+        resp.send(found);
+    else
+        resp.status(404).send();
     resp.end();
 });
 
 app.delete('/ad/:id', (req, resp) => {
-    const repository = new AdRepository();
-    const id = parseInt(req.param("id",0));
-    repository.delete(id);
+    const id = parseInt(req.params.id);
+    repository.delete({'id': id});
     resp.end();
 });
 
 app.get('/ad/search', (req, resp) => {
-    resp.write('not implemented');
+    resp.status(501).send('not implemented');
     resp.end();
 });
 
