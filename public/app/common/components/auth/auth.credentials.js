cc5999aed5a3eff1b3a590c72edf6705544d9d9c
Caio Silveira <caiocsilveira@gmail.com>
2017-10-04 23:05:08 -0300

    Update refresh user data without relogging

--- public/app/common/components/auth/auth.credentials.js
+++ public/app/common/components/auth/auth.credentials.js
@@ -8,11 +8,19 @@ class AuthCredentials {
   setToken(access_token) {
     let date = new Date();
     let expires_at = new Date(date.setDate(date.getDate() + 1));
-    access_token.isAdmin = parseInt(access_token.token.charAt(0)) == 1 ? true : false; 
+    access_token.isAdmin = parseInt(access_token.token.charAt(0)) == 1 ? true : false;
     this.$cookies.put('adf-access-token', JSON.stringify(access_token), {
       expires: expires_at,
       path: '/'
     });
+  }
+  updateToken(data){
+    let token = JSON.parse(this.currentAccessToken());
+    token.phone = data.phone;
+    token.name = data.name;
+    token.site = data.site;
+    token.region = data.region;
+    this.setToken(token);
   }
   clear() {
     this.$cookies.remove('adf-access-token', {
--- public/app/routes/profile/states/account/account.controller.js
+++ public/app/routes/profile/states/account/account.controller.js
@@ -1,11 +1,37 @@
 class ProfileAccountController {
-  constructor($scope) {
+  constructor($scope, $http, authCredentials, uiCommunication) {
     this.name = 'Profile Account Screen';
     this.$scope = $scope;
+    this.$http = $http;
+    this.authCredentials = authCredentials;
+    this.uiCommunication = uiCommunication;
     this.$scope.currentUser = $scope.$parent.$ctrl.currentUser;
+  }
+  save(){
+    let model = {
+      name: this.$scope.currentUser.name,
+      id: this.$scope.currentUser.cpf,
+      region: this.$scope.currentUser.region,
+      phone: this.$scope.currentUser.phone,
+      email: this.$scope.currentUser.email,
+      site: this.$scope.currentUser.site
+    };
+    this.$http.post('api://api/v1/profile/update/', angular.toJson(model)).then(response => {
+      this.uiCommunication.snackbar('Perfil salvo com sucesso !');
+      this.$http.get('api://api/v1/user/' + this.$scope.currentUser.email).then( response => {
+        this.authCredentials.updateToken(response.data);
+        this.$scope.$parent.$ctrl.getCurrentUser();
+      });
+    }, (response) => {
+      this.uiCommunication.snackbar('Não foi possível salvar os dados do perfil.');
+      if (response.data && response.data.error_description) {
+        console.log('error');
+      }
+    }), () => {
+    };
   }
 }
 
-ProfileAccountController.$inject = ['$scope'];
+ProfileAccountController.$inject = ['$scope', '$http', 'authCredentials', 'uiCommunication'];
 
 export default ProfileAccountController;
